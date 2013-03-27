cbd0f661695a44ed81617a2254665d20a4187b08
Dean Wenick <deanwenick@gmail.com>
2013-03-26 21:58:15 -0700

    fixed typos to show properly

--- views/contractors.php
+++ views/contractors.php
@@ -6,13 +6,13 @@
         echo '<p class="skills">';
         
         if( 'on' == $c->i_do_design ) {
-            echo('<span class="designer">Design</span>';
+            echo('<span class="designer">Design</span>');
         }
         if( 'on' == $c->i_do_theming ){
-            echo('<span class="theming">Theming</span>';
+            echo('<span class="theming">Theming</span>');
         }
         if( 'on' == $c->i_do_development ){
-            echo('<span class="developement">Development</span>';
+            echo('<span class="developement">Development</span>');
         }
         if( 'on' == $c->i_do_user_training ) {
             echo ('<span class="user-training">User Training</span>');
@@ -22,13 +22,13 @@
         echo '<p class="bio">'.$c->bio.'</p>';
         
         echo '<ul class="contractor-social">'; // Social links
-        echo '<li><a href="' . $c->website     .'">Website</a><li>';
-        echo '<li> <a href="'. $c->twitter_url .'">Twitter</a><li>';
-        echo '<li> <a href="'. $c->facebook_url.'">Facebook</a><li>';
-        echo '<li> <a href="'. $c->linkedin    .'">LinkedIn</a><li>';
-        echo '<li> <a href="'. $c->github_url  .'">Github</a><li>';
-        echo '<li> <a href="'. $c->wporg_url   .'">WordPress.org</a><li>';
-        echo '<li> <a href="'. $c->portfolio   .'">Portfolio</a><li>';
+        echo '<li><a href="' . $c->website     .'">Website</a></li>';
+        echo '<li> <a href="'. $c->twitter_url .'">Twitter</a></li>';
+        echo '<li> <a href="'. $c->facebook_url.'">Facebook</a></li>';
+        echo '<li> <a href="'. $c->linkedin    .'">LinkedIn</a></li>';
+        echo '<li> <a href="'. $c->github_url  .'">Github</a></li>';
+        echo '<li> <a href="'. $c->wporg_url   .'">WordPress.org</a></li>';
+        echo '<li> <a href="'. $c->portfolio   .'">Portfolio</a></li>';
         echo '</ul>';
     
     ?>
