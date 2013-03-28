5ff330c55a4b755540fefee0fc929551730b77a8
BandonRandon <bandonrandon@gmail.com>
2013-03-27 19:17:56 -0700

    Add Social Media Icons

--- views/contractors.php
+++ views/contractors.php
@@ -22,13 +22,13 @@
         echo ('<p class="bio">'.$c->bio.'</p>');
         
         echo ('<ul class="contractor-social">'); // Social links
-        echo ('<li><a href="' . $c->website     .'">Website</a></li>');
-        echo ('<li> <a href="'. $c->twitter_url .'">Twitter</a></li>');
-        echo ('<li> <a href="'. $c->facebook_url.'">Facebook</a></li>');
-        echo ('<li> <a href="'. $c->linkedin    .'">LinkedIn</a></li>');
-        echo ('<li> <a href="'. $c->github_url  .'">Github</a></li>');
-        echo ('<li> <a href="'. $c->wporg_url   .'">WordPress.org</a></li>');
-        echo ('<li> <a href="'. $c->portfolio   .'">Portfolio</a></li>');
+        echo ('<li class="website"><a href="' . $c->website     .'">Website</a></li>');
+        echo ('<li class="twitter" > <a href="'. $c->twitter_url .'">Twitter</a></li>');
+        echo ('<li class="facebook"> <a href="'. $c->facebook_url.'">Facebook</a></li>');
+        echo ('<li class="linkedin"> <a href="'. $c->linkedin    .'">LinkedIn</a></li>');
+        echo ('<li class="github"> <a href="'. $c->github_url  .'">Github</a></li>');
+        echo ('<li class="wporg"> <a href="'. $c->wporg_url   .'">WordPress.org</a></li>');
+        echo ('<li class="portfolio"> <a href="'. $c->portfolio   .'">Portfolio</a></li>');
         echo ('</ul>');
     
     ?>
